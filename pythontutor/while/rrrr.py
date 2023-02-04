023d963037626a218b3c9474b4c522aa019a19ee
sayana8 <122979736+sayana8@users.noreply.github.com>
2023-02-04 23:30:23 +0600

    Add files via upload

---
+++ pythontutor/while/rrrr.py
@@ -0,0 +1,13 @@
+#second max 
+first_max = int(input())
+second_max = int(input())
+if first_max < second_max:
+    first_max, second_max = second_max, first_max
+element = int(input())
+while element != 0:
+    if element > first_max:
+        second_max, first_max = first_max, element
+    elif element > second_max:
+        second_max = element
+    element = int(input())
+print(second_max)
